48fa547444152ddd053266b6eb1068f3d301e126
yash <yash240196@gmail.com>
2018-10-31 09:22:42 -0700

    added info about redux and components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,17 @@
 import { CONSTANT } from '../constants'
 
+//reducer for redux
 const performAction = (state = { val : 'value' }, action) => {
 
-    switch(action.type) {
+    switch(action.type) {   //check type of action performed
         
         case CONSTANT:
             console.log('state: ',state,' action ', action);
             let val = state.val
-            return { val }
+            return { val }     // return the value to change
         
         default:
-            return state
+            return state     
     }
 }
 
